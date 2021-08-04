dd4abb09d114a32590f7a92d74df0f586fddd5bb
Milad <mposhtdari@gmail.com>
2021-08-04 11:15:46 +0430

    added httprouter fileserver example 02

---
+++ c2/fileServer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main()  {
+	router := httprouter.New()
+
+	// Mapping to methods is possible with HttpRouter
+	router.ServeFiles("/static/*filepath", http.Dir("C:\\Users\\Miraddo\\go\\src\\go-restful\\c2\\fileServer\\static"))
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
