795d7f61634faac7967786df519ec301b6dbbabd
Milad <mposhtdari@gmail.com>
2021-08-04 10:54:09 +0430

    added httprouter example 01

---
+++ c2/httprouterExample/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	io "io"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func getCommandOutput(c string, args ...string) string  {
+	out, _ := exec.Command(c, args...).Output()
+
+	return string(out)
+}
+
+func goVersion(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
+	// I used windows path but for unix based system we can use this path /usr/local/go/bin/go
+	res := getCommandOutput("C:\\Program Files\\Go\\bin\\go.exe", "version")
+	_, err := io.WriteString(w, res)
+	if err != nil{
+		panic(err)
+	}
+	return
+}
+
+func getFileContent(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
+	_, err := fmt.Fprintf(w, getCommandOutput("cat", p.ByName("name")))
+	if err != nil {
+		return 
+	}
+}
+
+func main()  {
+	router := httprouter.New()
+	router.GET("/api/v1/go-version", goVersion)
+	router.GET("/api/v1/show-file/:name", getFileContent)
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Miraddo/go-restful
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
