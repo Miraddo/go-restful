2c72c394e443587277decaf665f57657b4fc48dc
Milad <mposhtdari@gmail.com>
2021-08-10 10:10:26 +0430

    Chaptar 8 : using flag for command line purpose

---
+++ c8/basic/flagExample.go
@@ -0,0 +1,16 @@
+//package main
+//
+//import (
+//	"flag"
+//	"log"
+//)
+//
+//var name = flag.String("name", "stranger", "your wonderfulname")
+//
+//
+//func main()  {
+//	flag.Parse()
+//
+//	log.Printf("Hello %s, Welcome to the command line world", *name)
+//}
+//
